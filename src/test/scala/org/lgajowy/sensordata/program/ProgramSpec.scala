9e63b3c321ef7064b21d2533d1412f77956a34a2
Lukasz Gajowy <lukasz.gajowy@adverity.com>
2021-11-24 14:11:08 +0100

    Test the whole program

---
+++ src/test/scala/org/lgajowy/sensordata/program/ProgramSpec.scala
@@ -0,0 +1,43 @@
+package org.lgajowy.sensordata.program
+
+import cats.effect.IO
+import cats.effect.testing.scalatest.AsyncIOSpec
+import org.lgajowy.sensordata.algebras._
+import org.lgajowy.sensordata.programs.Program
+import org.lgajowy.sensordata.testutils.TestConsole
+import org.scalatest.flatspec.AsyncFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class ProgramSpec extends AsyncFlatSpec with AsyncIOSpec with Matchers {
+
+  it should "read a directory and print the aggregated result" in {
+
+    val testConsole: TestConsole = new TestConsole()
+
+    val program = Program[IO](
+      CommandLineArgumentsParser.make[IO](),
+      CsvFileFinder.make[IO](),
+      SensorDataCollector.make[IO](),
+      SensorStatsCalculator.make[IO](),
+      StatsPrinter.make[IO](testConsole)
+    )
+
+    program
+      .run(List("src/test/resources/csvExamples"))
+      .asserting(
+        _ =>
+          testConsole.lines shouldBe Vector(
+            "Num of processed files: 3",
+            "Num of failed files: 0",
+            "Num of processed measurements: 12",
+            "Num of failed measurements: 6",
+            "Sensors with highest avg humidity:",
+            "sensor-id,min,avg,max",
+            "s2,1,61,88",
+            "s1,10,54,98",
+            "s3,NaN,NaN,NaN"
+          )
+      )
+  }
+
+}
