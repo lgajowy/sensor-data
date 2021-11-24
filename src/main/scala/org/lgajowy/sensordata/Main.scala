42ffdb3c95c8c0bb9b1e0978011836b533e5423e
Lukasz Gajowy <lukasz.gajowy@adverity.com>
2021-11-24 15:11:28 +0100

    Implement the main class

--- src/main/scala/org/lgajowy/sensordata/Main.scala
+++ src/main/scala/org/lgajowy/sensordata/Main.scala
@@ -1,3 +1,17 @@
 package org.lgajowy.sensordata
 
-object Main {}
+import cats.effect.{ ExitCode, IO, IOApp }
+import org.lgajowy.sensordata.algebras._
+import org.lgajowy.sensordata.programs.Program
+
+object Main extends IOApp {
+
+  override def run(args: List[String]): IO[ExitCode] =
+    Program[IO](
+      CommandLineArgumentsParser.make[IO](),
+      CsvFileFinder.make[IO](),
+      SensorDataCollector.make[IO](),
+      SensorStatsCalculator.make[IO](),
+      StatsPrinter.make[IO](IO.consoleForIO)
+    ).run(args)
+}
